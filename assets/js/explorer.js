8434a3e8c315e3826e0fade31a09adcee3b3b662
GabSiccion <gabsiccion@outlook.com>
2022-11-20 22:19:13 +0800

    ppt embedded in exploration

--- assets/js/explorer.js
+++ assets/js/explorer.js
@@ -13,6 +13,25 @@ function displayCourse() {
 
 	document.getElementById("article-text").innerHTML =
 		courseData[course]["article-text"];
+
+	let frame = document.createElement("iframe");
+
+	let y;
+	if (course == "Computer-Science") {
+		y = 1;
+	} else {
+		y = 0;
+	}
+
+	let container = document.getElementById("ppt-container");
+	let width = container.offsetWidth;
+	let height = width / 1.8;
+
+	frame.src = courseData[course]["powerpoint-src"][y];
+	frame.width = width + "px";
+	frame.height = height + "px";
+
+	document.getElementById("ppt-container").appendChild(frame);
 }
 
 window.onload = displayCourse;
